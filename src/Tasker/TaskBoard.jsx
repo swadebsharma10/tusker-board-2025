8603447d0bc06633a874555706f5b6e6f0ad38f5
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-05-25 23:20:36 +0600

    added defaultTasks and set in default state

--- src/Tasker/TaskBoard.jsx
+++ src/Tasker/TaskBoard.jsx
@@ -1,18 +1,31 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Integration API",
+    description:
+      "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearchTask/>
+          <SearchTask />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction/>
-          <TaskList/>
+          <TaskAction />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
