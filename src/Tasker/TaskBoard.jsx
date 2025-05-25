c7feb03f8592b739b55290e64730a31080a1c7f0
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-05-25 23:39:40 +0600

    added showTask modal

--- src/Tasker/TaskBoard.jsx
+++ src/Tasker/TaskBoard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddTaskModal from "./Modal/AddTaskModal";
 
 const TaskBoard = () => {
   const defaultTask = {
@@ -15,16 +16,24 @@ const TaskBoard = () => {
   };
 
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+
+  const handleAddTask = ()=>{
+     console.log('add task btn clicked');
+     setShowAddModal(true)
+  }
 
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal></AddTaskModal>}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction />
+          <TaskAction handleAddTask={handleAddTask} />
           <TaskList tasks={tasks} />
         </div>
       </div>
--- src/Tasker/TaskList.jsx
+++ src/Tasker/TaskList.jsx
@@ -1,7 +1,8 @@
+import { FaStar } from "react-icons/fa";
 
 
 const TaskList = ({tasks}) => {
-  console.log(tasks)
+  
       return (
            <div className="overflow-auto">
             <table className="table-fixed overflow-auto xl:w-full">
@@ -34,21 +35,9 @@ const TaskList = ({tasks}) => {
                 {
                   tasks.map(task =>  <tr key={task.id} className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
                   <td>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="icon icon-tabler icon-tabler-star"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      stroke-width="2"
-                      stroke="yellow"
-                      fill="yellow"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-                    </svg>
+                    {
+                      task.isFavorite ? <FaStar className="text-yellow-500" />: <FaStar className="text-gray-400" />
+                    }
                   </td>
                   <td>{task.title}</td>
                   <td>
