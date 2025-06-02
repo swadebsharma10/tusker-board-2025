0b5c7de653371d5458071d1f401c941848461186
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-06-02 22:31:52 +0600

    added modal for create a task

--- src/Tasker/Modal/AddTaskModal.jsx
+++ src/Tasker/Modal/AddTaskModal.jsx
@@ -1,39 +1,84 @@
 const AddTaskModal = () => {
+
+
+  
+
+
+
+
+
+
   return (
-    <form>
-      <div className="flex">
-        <div className="relative overflow-hidden rounded-lg text-gray-50 md:min-w-[380px] lg:min-w-[440px]">
-          <input
-            type="search"
-            id="search-dropdown"
-            className="z-20 block w-full bg-gray-800 px-4 py-2.5 pr-10 focus:outline-none"
-            placeholder="Search Task"
-            required
-          />
-          <button
-            type="submit"
-            className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-3"
-          >
-            <svg
-              className="h-4 w-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+    <>
+      <div className="bg-black z-10 opacity-85 h-full w-full absolute top-0 left-0">
+        <form className="z-10 absolute top-1/4 left-1/3 mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
+          <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
+            Add New Task
+          </h2>
+
+          <div className="space-y-9 text-white lg:space-y-10">
+            <div className="space-y-2 lg:space-y-3">
+              <label for="title">Title</label>
+              <input
+                className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+                type="text"
+                name="title"
+                id="title"
+                required
               />
-            </svg>
-            <span className="sr-only">Search</span>
-          </button>
-        </div>
+            </div>
+
+            <div className="space-y-2 lg:space-y-3">
+              <label for="description">Description</label>
+              <textarea
+                className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
+                type="text"
+                name="description"
+                id="description"
+                required
+              ></textarea>
+            </div>
+
+            <div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
+              <div className="space-y-2 lg:space-y-3">
+                <label for="tags">Tags</label>
+                <input
+                  className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+                  type="text"
+                  name="tags"
+                  id="tags"
+                  required
+                />
+              </div>
+
+              <div className="space-y-2 lg:space-y-3">
+                <label for="priority">Priority</label>
+                <select
+                  className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
+                  name="priority"
+                  id="priority"
+                  required
+                >
+                  <option value="">Select Priority</option>
+                  <option value="low">Low</option>
+                  <option value="medium">Medium</option>
+                  <option value="high">High</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-16 flex justify-center lg:mt-20">
+            <button
+              type="submit"
+              className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            >
+              Create new Task
+            </button>
+          </div>
+        </form>
       </div>
-    </form>
+    </>
   );
 };
 
--- src/Tasker/TaskBoard.jsx
+++ src/Tasker/TaskBoard.jsx
@@ -33,7 +33,7 @@ const TaskBoard = () => {
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction handleAddTask={handleAddTask} />
+          <TaskAction handleAddTask={()=> setShowAddModal(true)} />
           <TaskList tasks={tasks} />
         </div>
       </div>
